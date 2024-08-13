d673ee6c197f54955ed8b2f7c7979d7099e30c6a
Kolppan3n <7803790+Kolppan3n@users.noreply.github.com>
2024-08-13 13:23:23 +0300

    Facebook and instagram SSO work. Added profile page to view session info

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -7,8 +7,17 @@ const profile = async () => {
   console.log(sessionString)
 
   return (
-    <main className="flex items-start justify-center">
-      <div>{sessionString ? sessionString : "No Session or cant get Session"}</div>
+    <main className="flex items-center justify-center">
+      {session ? (
+        <div className="flex flex-col items-center">
+          <p>{`Name: ${session.user?.name}`}</p>
+          <p>{`Email: ${session.user?.email}`}</p>
+          <p>{`ID: ${session.user?.id}`}</p>
+          <img className="" src={session?.user?.image ? session.user.image : ""} />
+        </div>
+      ) : (
+        <></>
+      )}
     </main>
   )
 }
--- auth.ts
+++ auth.ts
@@ -1,8 +1,8 @@
 import NextAuth from "next-auth"
-import google from "next-auth/providers/google"
-import facebook from "next-auth/providers/facebook"
-import instagram from "next-auth/providers/instagram"
+import Google from "next-auth/providers/google"
+import Facebook from "next-auth/providers/facebook"
+import Instagram from "next-auth/providers/instagram"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [google, facebook, instagram],
+  providers: [Google, Facebook, Instagram],
 })
